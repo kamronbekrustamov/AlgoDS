79d742db4c2f52b7722c826ff372a7fe5d309bae
Kamronbek Rustamov <kamronbekrustamov1@gmail.com>
2021-07-14 21:28:31 +0500

    Solutions to easy and medium problems

---
+++ src/problems/easy/ReverseInteger.java
@@ -0,0 +1,20 @@
+package problems.easy;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        System.out.println(reverse(-1463847412));
+    }
+    public static int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int lastDigit = x % 10;
+            x /= 10;
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && lastDigit > 7))
+                return 0;
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && lastDigit < -8))
+                return 0;
+            result = result * 10 + lastDigit;
+        }
+        return result;
+    }
+}
